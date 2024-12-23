e1b3a1f70822e22b399a75512c9b838e54e35412
sakshamwithweb <websaksham39@gmail.com>
2024-12-23 10:22:23 +0530

    added map key in skill

--- components/Knowledge.js
+++ components/Knowledge.js
@@ -12,9 +12,9 @@ const Knowledge = () => {
         <div id='knowledge' className='flex flex-col justify-center items-center border-b'>
             <h1 className='text-3xl font-bold underline'>Knowledge</h1>
             <div className='my-24 grid grid-cols-1 md:grid-cols-2 md:gap-16 gap-6'>
-                {skills.map((i) => {
+                {skills.map((i,index) => {
                     return (
-                        <div className='md:w-80 w-64'>
+                        <div className='md:w-80 w-64' key={index}>
                             <strong>{i.skill}</strong>
                             <Progress value={i.percentage} />
                         </div>
