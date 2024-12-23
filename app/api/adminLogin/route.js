a7384afcd43091ab5be7bc45ec03207340fa49dc
sakshamwithweb <websaksham39@gmail.com>
2024-12-23 09:45:32 +0530

    encrypt password of admin using  bcrypt

--- app/api/adminLogin/route.js
+++ app/api/adminLogin/route.js
@@ -1,19 +1,31 @@
 import { Admin } from "@/lib/models/admin/admin"
 import connectDb from "@/lib/mongoose"
 import { NextResponse } from "next/server"
+import bcrypt from "bcrypt"
 
+
+async function CheckHashPass(userPass, hashPass) {
+    try {
+        const result = await bcrypt.compare(userPass, hashPass);
+        return result;
+    } catch (err) {
+        console.error("Error comparing passwords:", err);
+        return false;
+    }
+}
 
 export async function POST(req) {
     try {
         const { userName, pass } = await req.json()
         console.log(userName, pass)
-        // check if true or else return an err
         await connectDb()
         const admin = await Admin.findOne({ userName: userName })
-        if (!admin) throw new Error("Wrong credentials");
-        if (admin.pass != pass) throw new Error("Wrong credentials");
+        if (!admin) return NextResponse.json({ success: false, error: "Wrong credentials" })
+        const checkPass = await CheckHashPass(pass, admin.pass)
+        if (checkPass != true) return NextResponse.json({ success: false, error: "Wrong credentials" })
         return NextResponse.json({ success: true, userName: admin.userName })
+
     } catch (error) {
-        return NextResponse.json({ success: false, error: error.message })
+        return NextResponse.json({ success: false, error: `Server error, contact:${process.env.NEXT_PUBLIC_EMAIl}` })
     }
 }
