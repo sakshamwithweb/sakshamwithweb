63936653ef987eee890fd09453773827ec37bf35
sakshamwithweb <websaksham39@gmail.com>
2025-02-01 17:02:37 +0530

    Made navbar for both user and admin

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
 import {
   Sheet,
@@ -11,16 +11,76 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { ModeToggle } from './ui/mode'
-
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const [logged, setLogged] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const req = await fetch("/api/checkSession", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+      })
+      const res = await req.json()
+      if (!res.success) {
+        setLogged(false)
+      } else {
+        setLogged(true)
+      }
+    }
+    checkSession()
+  }, [])
+
+  if (!logged) {
+    return (
+      <nav className='flex mx-auto sticky top-0 p-2 md:p-2 justify-between items-center backdrop-blur border border-b-gray-400/50 z-50'>
+        <div className='font-bold text-xl mx-2 md:mx-4'>
+          <Link href={"/"}>SakshamWithWeb</Link>
+        </div>
+        <div className='flex'>
+          <span className='hidden md:block'> <ModeToggle /></span>
+          {/*For Smaller device*/}
+          <div className='md:hidden mx-4'>
+            <span className='mx-4'> <ModeToggle /></span>
+            <Sheet>
+              <SheetTrigger aria-label='hamburger'>
+                <RxHamburgerMenu />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>SakshamWithWeb</SheetTitle>
+                  <SheetDescription>
+                    <Link className='' href={`/#main`}>Home</Link>
+                    <Link href={`/#about`}>About</Link>
+                    <Link href={`/#knowledge`}>Knowledge</Link>
+                    <Link className='' href={`/#projects`}>Projects</Link>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+
+          {/*For biger device*/}
+          <ul className='md:flex md:mt-2 gap-3 mx-4 hidden'>
+            <li><Link href={`/#main`}>Home</Link></li>
+            <li><Link href={`/#about`}>About</Link></li>
+            <li><Link href={`/#knowledge`}>Knowledge</Link></li>
+            <li><Link href={`/#projects`}>Projects</Link></li>
+          </ul>
+        </div>
+      </nav >
+    )
+  }
+
   return (
-    <nav className='flex mx-auto sticky top-0 p-2 md:p-2 justify-between items-center backdrop-blur border border-b-gray-400/50 z-50'>
-      <div className='font-bold text-xl mx-2 md:mx-4'>
-        <Link href={"/"}>SakshamWithWeb</Link>
-      </div>
+    <nav className='backdrop-blur sticky top-0 py-2 md:px-11 px-2 border-b flex items-center justify-between'>
+      <Link className='font-bold text-xl' href={"/admin/dashboard"}>SakshamWithWeb</Link>
       <div className='flex'>
-        <span className='hidden md:block'> <ModeToggle /></span>
         {/*For Smaller device*/}
         <div className='md:hidden mx-4'>
           <span className='mx-4'> <ModeToggle /></span>
@@ -32,10 +92,9 @@ const Navbar = () => {
               <SheetHeader>
                 <SheetTitle>SakshamWithWeb</SheetTitle>
                 <SheetDescription>
-                  <Link className='' href={`/#main`}>Home</Link>
-                  <Link href={`/#about`}>About</Link>
-                  <Link href={`/#knowledge`}>Knowledge</Link>
-                  <Link className='' href={`/#projects`}>Projects</Link>
+                  <Link className={`${pathname.includes("/dashboard") && "underline font-bold"}`} href={"/admin/dashboard"}>Details</Link>
+                  <Link className={`${pathname.includes("/blogs") && "underline font-bold"}`} href={"/admin/blogs"}>Blogs</Link>
+                  <Link className={`${pathname.includes("/queries") && "underline font-bold"}`} href={"/admin/queries"}>Queries</Link>
                 </SheetDescription>
               </SheetHeader>
             </SheetContent>
@@ -43,15 +102,16 @@ const Navbar = () => {
         </div>
 
         {/*For biger device*/}
-        <ul className='md:flex md:mt-2 gap-3 mx-4 hidden'>
-          <li><Link href={`/#main`}>Home</Link></li>
-          <li><Link href={`/#about`}>About</Link></li>
-          <li><Link href={`/#knowledge`}>Knowledge</Link></li>
-          <li><Link href={`/#projects`}>Projects</Link></li>
+        <ul className='gap-3 md:flex hidden justify-center items-center'>
+          <ModeToggle />
+          <Link className={`${pathname.includes("/dashboard") && "underline font-bold"}`} href={"/admin/dashboard"}>Details</Link>
+          <Link className={`${pathname.includes("/blogs") && "underline font-bold"}`} href={"/admin/blogs"}>Blogs</Link>
+          <Link className={`${pathname.includes("/queries") && "underline font-bold"}`} href={"/admin/queries"}>Queries</Link>
         </ul>
       </div>
-    </nav >
+    </nav>
   )
+
 }
 
 export default Navbar
--- doLater.md
+++ doLater.md
@@ -1,1 +1,4 @@
 Ensure API request is being sent from this website
+
+
+Instead of making many pages after admin, make one dynamic page then bring component and configure for admin at oncee
