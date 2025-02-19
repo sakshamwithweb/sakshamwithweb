b8009b110d4f66523b2deaa2638c191606926128
sakshamwithweb <websaksham39@gmail.com>
2025-02-19 21:05:19 +0530

    did:instead of making many nextrespone.json(success:FALSE) throw error then catch in catch

--- app/api/fetchAdminDetails/route.js
+++ app/api/fetchAdminDetails/route.js
@@ -6,7 +6,7 @@ export async function POST() {
     try {
         await connectDb()
         const data = await AdminDetails.findOne({ name: "Saksham" })
-        if (!data) return NextResponse.json({ success: false, message: "Unable to fetch data" })
+        if (!data) throw new Error("Unable to get data");
         return NextResponse.json({ data: data, success: true })
     } catch (error) {
         return NextResponse.json({ success: false, message: "Unable to fetch data" })
