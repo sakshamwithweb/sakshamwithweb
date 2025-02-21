84b5922c52756a999d11644ff414893f1682cfa9
sakshamwithweb <websaksham39@gmail.com>
2025-02-21 10:31:30 +0530

    changed color of loading

--- components/Loader.js
+++ components/Loader.js
@@ -3,7 +3,7 @@ import { PacmanLoader } from "react-spinners";
 export function Loader() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <PacmanLoader size={20} color="purple" />
+      <PacmanLoader size={20} color="#9333EA" />
     </div>
   );
 }
